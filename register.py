7b68088b40a895ea7b37addcf72f89eca8a94456
0x3-lua <93447100+0x3-lua@users.noreply.github.com>
2025-11-26 15:37:32 -0800

    convert_file test and pubd on github because vsc broke

--- register.py
+++ register.py
@@ -1,12 +1,13 @@
 import SimpleITK as sitk
 
-# converts a path leading to a dicom file to an image
+# converts a path leading to a directory of dicom files
+# to an image
 def convert_file(path):
 	reader = sitk.ImageSeriesReader()
 
 	reader.SetFileNames(reader.GetGDCMSeriesFileNames(path))
 
-	return reader.execute()
+	return reader.Execute()
 
 
 def attempt_register(fixed, moving): 
@@ -16,3 +17,9 @@ def attempt_register(fixed, moving):
 	#apply demons
 	#test #iterations
 
+
+patient1 = "/home/a/main/Dataset/Data/Patient1"
+patient2 = "/home/a/main/Dataset/Data/Patient2"
+
+sitk.Show(convert_file(patient1))
+sitk.Show(convert_file(patient2))
