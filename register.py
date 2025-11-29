a2a70e48ce82f50dc3682c15d2832f341270c6a1
0x3-lua <93447100+0x3-lua@users.noreply.github.com>
2025-11-29 09:09:17 -0800

    test

--- register.py
+++ register.py
@@ -16,20 +16,21 @@ def attempt_register(fixed, moving):
 	img_fixed = convert_file(fixed)
 	img_moving = convert_file(moving)
 
+	#print(img_fixed.GetPixelID()) # 4
+
 	matcher = sitk.HistogramMatchingImageFilter()
 	matcher.SetNumberOfHistogramLevels(1024)
 	matcher.SetNumberOfMatchPoints(7)
 	matcher.ThresholdAtMeanIntensityOn()
 
 	img_moving = matcher.Execute(img_moving, img_fixed)
-
+	
 	#apply demons
-	demons = sitk.DemonsRegistrationFilter()
-	demons.SetNumberOfIterations(50)
+	demons = sitk.FastSymmetricForcesDemonsRegistrationFilter()
+	demons.SetNumberOfIterations(5)
 	demons.SetStandardDeviations(1.0)
 
-	displacement_field = demons.Execute(img_fixed, img_moving)
-
+	#displacement_field = demons.Execute(img_fixed, img_moving)
 	print("check")
 
 patient1 = "/home/a/main/Dataset/Data/Patient1"
